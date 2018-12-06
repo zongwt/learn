bca4c48122797d5baa3c0dde51f5a10d6656cb38
zongwt <zong_weitao@sina.com>
2018-12-06 22:59:44 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import annotation.MyAnnotation;
+
+@MyAnnotation(color="red")
+public class Main {
+
+    public static void main(String[] args) {
+        MyAnnotation myAnnotation = Main.class.getAnnotation(MyAnnotation.class);
+        System.out.println(myAnnotation.color());
+    }
+}
